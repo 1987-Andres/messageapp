a3e92d466b3de3e65364a40c83444f52946ce3ce
Mella <melladjs@hotmail.com>
2022-01-23 00:05:04 +0100

    eliminacion ruta duplicada

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
-import { MensajesController } from './mensajes/mensajes.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
